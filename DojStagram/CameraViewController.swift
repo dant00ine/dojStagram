702bd4fb5c7ea7dc3d2e568392d0de6067f555f2
Daniel Thompson <danthwa@gmail.com>
2017-03-17 11:36:16 -0700

    Initial Commit

---
+++ DojStagram/CameraViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  DojStagram
+//
+//  Created by Daniel Thompson on 3/17/17.
+//  Copyright © 2017 Daniel Thompson. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+    
+    
+    @IBOutlet weak var cameraView: UIView!
+    
+    let captureSession = AVCaptureSession()
+    var sessionOutput = AVCapturePhotoOutput()
+    var previewLayer = AVCaptureVideoPreviewLayer()
+
+    
+    @IBAction func takePhoto(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let deviceSession = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInDualCamera, .builtInTelephotoCamera, .builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: .unspecified)
+        
+        for device in (deviceSession?.devices)! {
+            
+            if device.position == AVCaptureDevicePosition.front {
+                
+                do {
+                    
+                    let input = try AVCaptureDeviceInput(device: device)
+                    
+                    if captureSession.canAddInput(input){
+                        
+                        captureSession.addInput(input)
+                        
+                        if captureSession.canAddOutput(sessionOutput) {
+                            captureSession.addOutput(sessionOutput)
+                            
+                            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                            previewLayer.connection.videoOrientation = .portrait
+                            
+                            cameraView.layer.addSublayer(previewLayer)
+//                            cameraView.addSubview(button?)
+                            previewLayer.position = CGPoint(x: self.cameraView.frame.width / 2, y: self.cameraView.frame.height / 2)
+                            previewLayer.bounds = cameraView.frame
+                            
+                            captureSession.startRunning()
+                            
+                        }
+                        
+                    }
+                    
+                } catch let avError {
+                    print(avError)
+                }
+                
+            }
+            
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
