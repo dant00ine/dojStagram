85e686ed51da1f5bab42d0fbce8b62ba8e5f1583
pbinneboese <pbinneboese@mac.com>
2017-03-30 20:32:21 -0700

    more filter tweaks

--- DojStagram/EditViewController.swift
+++ DojStagram/EditViewController.swift
@@ -45,6 +45,11 @@ class EditViewController: UIViewController {
         }
         context = CIContext()
         currentFilter = CIFilter(name: "CISepiaTone")
+        let beginImage = CIImage(image: currentImage!)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -89,12 +94,11 @@ class EditViewController: UIViewController {
         if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
         if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
         if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage!.size.width / 2, y: currentImage!.size.height / 2), forKey: kCIInputCenterKey) }
-//        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
         
         if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
             let processedImage = UIImage(cgImage: cgimg)
             // display the processed image
-            imageView.image = processedImage
+            self.imageView.image = processedImage
         }
     }
 
